27bdc5067221e90cc37832cc6348f48a5e47e8ce
czerkisi <czerkisi@msoe.edu>
2023-02-16 17:52:41 -0600

    beginning react interface

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
 import React from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import DashboardPage from './components/Dashboard';
 import CreateAccountPage from './components/CreateAccount';
 
 function App() {
   return (
-      <Router>
-        <div>
-          <Switch>
-            <Route path="/" component={LoginPage} />
-            <Route path="/dashboard" component={DashboardPage} />
-            <Route path="/create-account" component={CreateAccountPage} />
-          </Switch>
-        </div>
-      </Router>
+      <div className="wrapper">
+          <h1>Application</h1>
+          <BrowserRouter>
+              <Switch>
+                  <Route path="/" component={LoginPage} />
+                  <Route path="/dashboard" component={DashboardPage} />
+                  <Route path="/create-account" component={CreateAccountPage} />
+              </Switch>
+          </BrowserRouter>
+      </div>
   );
 }
 
---
+++ src/components/Dashboard.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class DashBoard extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return(
+            <h2>Dashboard</h2>
+        );
+    }
+}
+
+export default Dashboard;
